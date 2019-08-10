861e7c4d2335fc4b7eb4bb0be3ae9a8a514910b0
Darren Campbell <darren2802@gmail.com>
2019-08-10 13:53:19 +0200

    Update Day 7 Work

--- day_7/checker_board.rb
+++ day_7/checker_board.rb
@@ -3,26 +3,14 @@ cb_size = gets.chomp.to_i
 
 for i in 1..cb_size
   if i % 2 == 0
-    indent_x = true
-  else
-    indent_x = false
+    print " "
   end
 
-  if indent_x == true
-    for j in 1..cb_size
-      if j % 2 == 0
-        print "X"
-      else
-        print " "
-      end
-    end
-  else
-    for j in 1..cb_size
-      if j % 2 == 0
-        print " "
-      else
-        print "X"
-      end
+  for j in 1..cb_size
+    if j % 2 != 0
+      print "X"
+    else
+      print " "
     end
   end
 
--- day_7/fizzbuzz.rb
+++ day_7/fizzbuzz.rb
@@ -3,14 +3,28 @@ fb_start = gets.chomp.to_i
 print "Please enter the end number for the FizzBuzz range: "
 fb_end = gets.chomp.to_i
 
+puts ""
+counter = 0
+row_counter = 1
+
 for i in fb_start..fb_end
+  counter += 1
   if i % 3 == 0 && i % 5 == 0
-    puts "FizzBuzz"
+    print "FizzBuzz"
   elsif i % 3 == 0
-    puts "Fizz"
+    print "Fizz"
   elsif i % 5 == 0
-    puts "Buzz"
+    print "Buzz"
   else
-    puts i
+    print i
+  end
+  if counter == row_counter
+    row_counter += 1
+    counter = 0
+    puts ""
+  else
+    print " "
   end
 end
+
+puts ""
